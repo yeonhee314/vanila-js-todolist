ad97f29d3ea9a525f5d7451a1ca0d3d641059e6b
yeonhee <mouse97_@naver.com>
2025-01-01 21:40:47 +0900

    로컬 스토리지에 유저 네임 저장

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,17 @@
-alert('hi');
+const loginForm = document.querySelector("#login-form");
+const loginInput = document.querySelector("#login-form input");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden";
+
+function onLoginSubmit(event){
+    event.preventDefault();
+    const userName = loginInput.value;
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    console.log(userName);
+    localStorage.setItem("userName", userName);
+    greeting.classList.remove(HIDDEN_CLASSNAME);
+    greeting.innerText = `Hello ${userName}`;
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
